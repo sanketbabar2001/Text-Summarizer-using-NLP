841cd16c6616f4b0705650ed67a249c52d5ffc6b
sanketbabar2001 <sanketbabar2001@gmail.com>
2023-08-30 17:37:05 +0530

    data validation completed

--- src/TextSummarizer/entity/__init__.py
+++ src/TextSummarizer/entity/__init__.py
@@ -4,6 +4,12 @@ from pathlib import Path
 @dataclass(frozen=True)
 class DataIngestionConfig:
     root_dir: Path
-    
     local_data_file: Path
     unzip_dir: Path
+
+
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir: Path
+    STATUS_FILE: str
+    ALL_REQUIRED_FILES: list
