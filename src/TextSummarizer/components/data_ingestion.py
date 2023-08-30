0639b4060b43977e54c6a23f10b0640449ade0e9
sanketbabar2001 <sanketbabar2001@gmail.com>
2023-08-30 16:37:16 +0530

    data ingestion notebook added

---
+++ src/TextSummarizer/components/data_ingestion.py
@@ -0,0 +1,18 @@
+import os
+import urllib.request as request
+import zipfile
+from textSummarizer.logging import logger
+from textSummarizer.utils.common import get_size
+from pathlib import Path
+from textSummarizer.entity import DataIngestionConfig
+
+
+class DataIngestion:
+    def __init__(self, config: DataIngestionConfig):
+        self.config = config
+
+    def extract_zip_file(self):
+        unzip_path = self.config.unzip_dir
+        os.makedirs(unzip_path, exist_ok=True)
+        with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
+            zip_ref.extractall(unzip_path)
